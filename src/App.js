56ca1c11572fa3e38c4141574ec974321f36b17a
Victoria  Zavertaliuk <vikvv@ukr.net>
2023-02-23 22:45:57 +0200

    add functions handleChange

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
    
-    <Diary/>
+    <Diary defaultCity = 'Kyiv'/>
     </div>
   );
 }
--- src/Diary.js
+++ src/Diary.js
@@ -1,13 +1,55 @@
-import React from "react";
+import React, { useState} from "react";
 import "./Diary.css";
 
-export default function Diary() {
+export default function Diary(props) {
+   const [city, setCity] = useState(props.defaultCity);
+   
+
+  const handleMonthChange = event => {
+    console.log(event.target.value);
+    setMonth(event.target.value);
+  };
+  const handleYearChange = event => {
+   console.log(event.target.value);
+   setYear(event.target.value);
+ };
+   const months = [
+      {value: 'Січень', text: 'Січень'},
+      {value: 'Лютий', text: 'Лютий'},
+      {value: 'Березень', text: 'Березень'},
+      {value: 'Квітень', text: 'Квітень'},
+      {value: 'Червень', text: 'Червень'},
+      {value: 'Липень', text: 'Липень'},
+      {value: 'Серпень', text: 'Серпень'},
+      {value: 'Вересень', text: 'Вересень'},
+      {value: 'Жовтень', text: 'Жовтень'},
+      {value: 'Листопад', text: 'Листопад'},
+      {value: 'Грудень', text: 'Грудень'},
+    ];
+    const years = [
+      {value: '2023', text: '2023'},
+      {value: '2024', text: '2024'},
+      {value: '2025', text: '2025'},
+      {value: '2026', text: '2026'},
+      {value: '2027', text: '2027'},
+      {value: '2028', text: '2028'},
+      {value: '2030', text: '2030'},
+      {value: '2031', text: '2031'},
+      {value: '2032', text: '2032'},
+      {value: '2033', text: '2033'},
+     
+     
+    ];
+    const [month, setMonth] = useState(months[0].value);
+    const [year, setYear] = useState(years[0].value);
   function handleSubmit(params) {
    
   }
-  function handleChange(params) {
+  function handleCityChange(event) {
+   console.log(city);
+   setCity(event.target.value);
+   }
    
-}
    return (
       <div className="container">
       <div className="wrapper">
@@ -16,46 +58,33 @@ export default function Diary() {
          <form className="pb-3" onSubmit={handleSubmit}>
          <div className="row">
                <div className="col-3">
-   <input type="text" className="input form-control" placeholder="Enter a city" autoFocus ="on" onChange={handleChange}/>
+   <input type="text" className="input form-control" placeholder="Enter a city" autoFocus ="on" onChange={handleCityChange}/>
 </div>
 <div className="col-3">
-   <select name="select">
-
-  <option value="value1" selected> Січень</option>
-  <option value="value2" >Лютий</option>
-  <option value="value3">Березень</option>
-  <option value="value3">Квітень</option>
-  <option value="value3">Травень</option>
-  <option value="value3">Червень</option>
-  <option value="value3">Липень</option>
-  <option value="value3">Серпень</option>
-  <option value="value3">Вересень</option>
-  <option value="value3">Жовтень</option>
-  <option value="value3">Листопад</option>
-  <option value="value3">Грудень</option>
-</select>
+<select value={month} onChange={handleMonthChange}>
+        {months.map(month => (
+          <option key={month.value} value={month.value}>
+            {month.text}
+          </option>
+        ))}
+      </select>
 </div>
 <div className="col-3">
-<select name="select">
-
-<option value="value1" selected> 2023</option>
-<option value="value2" >2024</option>
-<option value="value3">2025</option>
-<option value="value3">2026</option>
-<option value="value3">2027</option>
-<option value="value3">2028</option>
-<option value="value3">2029</option>
-<option value="value3">2030</option>
-<option value="value3">2031</option>
-<option value="value3">2032</option>
-<option value="value3">2033</option>
-
-</select>
+<select value={year} onChange={handleYearChange}>
+        {years.map(year => (
+          <option key={year.value} value={year.value}>
+            {year.text}
+          </option>
+        ))}
+      </select>
 </div>
 
 <div className="col-3">
 <input className="btn btn-primary btn-search" type="submit" value="Get the diary" />
 </div>
+<p>{city}</p>
+<p>{month}</p>
+<p>{year}</p>
 </div>
 </form>
 </div>
