40889648c2b63f0442a3b1beb14d99d4b859557c
Victoria  Zavertaliuk <vikvv@ukr.net>
2023-02-24 22:48:05 +0200

    make api request coordinates

--- src/Diary.js
+++ src/Diary.js
@@ -1,5 +1,6 @@
 import React, { useState} from "react";
 import "./Diary.css";
+import axios from 'axios';
 
 export default function Diary(props) {
    const [city, setCity] = useState(props.defaultCity);
@@ -42,14 +43,56 @@ export default function Diary(props) {
     ];
     const [month, setMonth] = useState(months[0].value);
     const [year, setYear] = useState(years[0].value);
-  function handleSubmit(params) {
+    const [loaded,setLoaded]= useState(true);
+
+    function handleCoordResponse(response) {
+      let lon= response.data.coord.lon;
+      let lat =response.data.coord.lat;
+      console.log(lon);
+      console.log(lat);
+    }
+function searchCoordinates () {
+   let apiKey = "428a806b1ea72671015f9a8da5f82916";
+   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+   axios.get(url).then(handleCoordResponse);
+}
+
+
+
+    function handleResponse(response) {
+      setLoaded(true);
+      console.log(response.data.temp);
+      // setWeatherData({
+      //    coordinates: response.data.coord,
+      //    temperature :response.data.main.temp,
+      //    description: response.data.weather[0].description,
+      //    humidity: response.data.main.humidity,
+      //    wind: response.data.wind.speed,
+      //    icon: response.data.weather[0].icon,
+      //    date: new Date(response.data.dt*1000), 
+      //    city: response.data.name,
+      // });
+   }
+  function search(lat, lon) {
+   searchCoordinates(city);
+
+   let apiKey = "428a806b1ea72671015f9a8da5f82916";
+
+   let url = `https://api.openweathermap.org/data/3.0/onecall/timemachine?lat=${lat}&lon=${lon}&dt={time}&appid=${apiKey}`
    
-  }
+   axios.get(url).then(handleResponse);
+}
+function handleSubmit(event) {
+   event.preventDefault();
+   search();
+}
+
   function handleCityChange(event) {
    console.log(city);
    setCity(event.target.value);
    }
    
+   if (loaded) {
    return (
       <div className="container">
       <div className="wrapper">
@@ -92,3 +135,5 @@ export default function Diary(props) {
    </div>
    )
 }
+}
+ 
